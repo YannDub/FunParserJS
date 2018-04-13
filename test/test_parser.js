49350a4a8e9fa09e617c7fc92c27f527ad4a5fbb
Yann Dubois <ttreplay@gmail.com>
2018-04-13 19:28:11 +0200

    Update tests

--- test/test_parser.js
+++ test/test_parser.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const parser = require("../lib/parser");
+const parser = require("../index.js");
 
 describe("parser", () => {
   describe(".success", () => {
     it("simple", (done) => {
-      expect(parser.runParser(success("OK"), "foo")).to.equal(parser.Just("OK", "foo"));
+      expect(parser.runParser(parser.success("OK"), "foo")).to.be.an('array').that.includes('OK');
+      done();
     })
   })
 })
